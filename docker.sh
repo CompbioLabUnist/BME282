82282720caac5672fd0920cf75723165d2c8e5a6
Jaewoong Lee <230@fumire.moe>
2023-10-16 14:03:33 +0900

    Docker test on Mayr server

---
+++ docker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm -it --volume /BiO/Live/pbj1862/RNAseq_Example/Analysis/00.bamtofq/Cancer_Group:/Cancer:ro --volume /BiO/Live/pbj1862/RNAseq_Example/Analysis/00.bamtofq:/Normal:ro --volume /BiO2/Research/UNIST-KabukiSyndrome_PNUYH_CCK-2022-12/Resource/RSEM:/Reference/RSEM:ro --volume /BiO/Share/Tools/gatk-bundle/hg38:/Reference/gatk-bundle:ro --volume $(realpath .):/Working fumire/bme282:latest /bin/bash
